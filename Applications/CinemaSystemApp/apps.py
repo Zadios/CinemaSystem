361628a1959a69a434c5039ae61d08f4a85b0394
Ariel Viscovich <arielviscovich@gmail.com>
2024-05-27 12:14:50 -0300

    Movie & CinemaSystemApp Configuradas

--- Applications/CinemaSystemApp/apps.py
+++ Applications/CinemaSystemApp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CinemasystemappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CinemaSystemApp'
+    name = 'Applications.CinemaSystemApp'
