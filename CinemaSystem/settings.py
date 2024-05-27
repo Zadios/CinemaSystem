43267d6f519b6ad76b7c292db8a5f77a2922c1db
Ariel Viscovich <arielviscovich@gmail.com>
2024-05-27 11:38:29 -0300

    Conexión con postgreSQL establecida

--- CinemaSystem/settings.py
+++ CinemaSystem/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'CinemaSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'NAME': 'cinema_db',
     }
 }
 
