8a31dec7e496cbffc422e8a666f343c2e5401add
Ariel Viscovich <arielviscovich@gmail.com>
2024-05-27 13:16:17 -0300

    Movie/models.py : Pelicula agregada

--- Applications/Movie/models.py
+++ Applications/Movie/models.py
@@ -1,3 +1,13 @@
+from importlib.util import module_from_spec
 from django.db import models
 
 # Create your models here.
+
+class Pelicula(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField('Nombre', max_length=100, null=True)
+    sinopsis = models.TextField('Sinopsis', null=True)
+    imagen = models.ImageField('Imagen', upload_to='portadas/', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id)+ ' - ' + self.nombre
