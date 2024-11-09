7eaeb73614f942962164cdd9fb081b5ccb00aa88
aViscovichVippinn <aviscovich@iico.vippinn.com>
2024-11-08 21:20:32 -0300

    Cambiado nombre de comprar_entradas por horarios. Intento de integrar plantilla de horarios (errores)

--- Applications/Showtime/views.py
+++ Applications/Showtime/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from Applications.Movie.models import Film, Format, Language
 from .models import Show, Movie_Theater
 
-def comprar_entradas(request, film_id):
+def horarios(request, film_id):
     film = get_object_or_404(Film, pk=film_id)
     formats = film.format.all()  # Formatos disponibles para la película
     languages = film.language.all()  # Idiomas disponibles para la película
-    shows = Show.objects.filter(film=film)
+    shows = Show.objects.filter(film=film).order_by('show_date', 'show_time')
 
     selected_format = request.GET.get('format')
     selected_language = request.GET.get('language')
@@ -17,6 +17,15 @@ def comprar_entradas(request, film_id):
     if selected_language:
         shows = shows.filter(language_id=selected_language)
 
+    # Organizar los horarios por fecha
+    horarios_por_fecha = {}
+    for show in shows:
+        fecha = show.show_date.strftime('%Y-%m-%d')
+        hora = show.show_time.strftime('%H:%M')
+        if fecha not in horarios_por_fecha:
+            horarios_por_fecha[fecha] = []
+        horarios_por_fecha[fecha].append(hora)
+
     context = {
         'film': film,
         'formats': formats,
@@ -24,5 +33,16 @@ def comprar_entradas(request, film_id):
         'shows': shows,
         'selected_format': selected_format,
         'selected_language': selected_language,
+        'horarios_por_fecha': horarios_por_fecha,  # Pasamos el diccionario de horarios al template
+    }
+    return render(request, 'showtime/horarios.html', context)
+
+def comprar_entradas(request, show_id):
+    # Obtener el show específico usando el show_id
+    show = get_object_or_404(Show, pk=show_id)
+
+    # Contexto para el template: puedes pasar cualquier dato necesario para la compra
+    context = {
+        'show': show,
     }
     return render(request, 'showtime/comprar_entradas.html', context)
